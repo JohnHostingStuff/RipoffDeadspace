8040b2dc5d2cd86aeee5693fd3e3c668c0deca54
Jack Brettell <126177788+xxJackinabox@users.noreply.github.com>
2024-03-04 18:42:37 +0000

    Terminal interaction
     - The player can now use the terminal
     - Simple placeholder menu

--- ACM2024GameJam/Assets/Scripts/TerminalProximity.cs
+++ ACM2024GameJam/Assets/Scripts/TerminalProximity.cs
@@ -2,22 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class TerminalProximity : MonoBehaviour
 {
 
+    // Prompt text
     public GameObject Prompt;
 
+    // To assign player camera
+    public GameObject playerCamera;
+
+    // Define the terminal's position 
+    public Transform targetPosition;
+
+    // Define playerbody position to return to
+    public Transform originalPosition;
+
+    // Disable MouseLook and movement script attached to player
+    public GameObject lookScriptObject;
+    public GameObject moveScriptObject;
+
+    private bool isCameraEnabled = true;
+    private bool isPlayerInRange = false;
+
+    //Terminal screen
+    public GameObject TerminalScreen;
+
+
+
+    void Start()
+    {
+        isCameraEnabled = !isCameraEnabled;
+
+        TerminalScreen.gameObject.SetActive(false);
+    }
 
     void OnTriggerEnter(Collider other)
     {
         // Check if the collider belongs to the player
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Player entered");
 
+            isPlayerInRange = true;
+           // Debug.Log("Player entered");
             Prompt.gameObject.SetActive(true);
-
         }
     }
 
@@ -26,9 +53,70 @@ public class TerminalProximity : MonoBehaviour
         // Check if the collider belongs to the player
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Player entered");
+            isPlayerInRange = false;
 
+            //Debug.Log("Player exited");
             Prompt.gameObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+
+
+        // Check for input E
+        if (Input.GetKeyDown(KeyCode.E) & isPlayerInRange)
+        {
+
+
+            // Toggle the camera script on/off
+            isCameraEnabled = !isCameraEnabled;
+
+            // If the camera is enabled, disable the MouseLook script
+            if (isCameraEnabled)
+            {
+            
+
+                MouseLook script = lookScriptObject.GetComponent<MouseLook>();
+                script.enabled = false;
+
+                PlayerMovement moveScript = moveScriptObject.GetComponent<PlayerMovement>();
+                moveScript.enabled = false;
+
+                Cursor.lockState = CursorLockMode.None;
+
+                TerminalScreen.gameObject.SetActive(true);
+
+
+                Prompt.gameObject.SetActive(false);
+
+                // Set the camera's position and rotation
+                playerCamera.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+                playerCamera.transform.position = targetPosition.position;
+            }
+            // If the camera is disabled, enable the MouseLook script
+            if (!isCameraEnabled)
+            {
+                // Re-enable the MouseLook script
+                MouseLook script = lookScriptObject.GetComponent<MouseLook>();
+                script.enabled = true;
+
+                PlayerMovement moveScript = moveScriptObject.GetComponent<PlayerMovement>();
+                moveScript.enabled = true;
+
+                Cursor.lockState = CursorLockMode.Locked;
+
+                TerminalScreen.gameObject.SetActive(false);
+
+                //  playerCamera.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+                playerCamera.transform.position = originalPosition.position;
+
+
+                
+
+                Prompt.gameObject.SetActive(true);
+
+            }
 
         }
     }
